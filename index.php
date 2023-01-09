34de0a47641228ed24f8efbb53925a285624c4d2
thanhdoan1201 <109785213+thanhdoan1201@users.noreply.github.com>
2023-01-09 14:59:38 +0700

    no care file

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-// you can add to the array
 $ext_array = array(".htm", ".php", ".asp", ".js"); //list of extensions not required
 $dir1 = "."; 
 $filecount1 = 0; 
@@ -29,12 +28,7 @@ $thelist .= '<LI><a href="'.$f1.'">'.$f1.'</a>';
 
 ?>
 
-<P> File</P>:</p>
+<P> File:</P></p>
 <UL>
 <P><?=$thelist?></p>
-<P> Anh Tú cáu</P></p>
-<video>
-    <source src="https://doanntph16820.000webhostapp.com/2023-01-07-20-29-04.mp4"
-            type='video/mp4 ' autoplay controls>
-</video>
 </UL>
